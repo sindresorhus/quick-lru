9a5f1c021a100be5aa3a47cfe6c5a5c25b38d84e
silverwind <me@silverwind.io>
2020-10-12 18:38:59 +0200

    Clarify docs on the iterable (#26)
    Co-authored-by: Pedro Augusto de Paula Barbosa <papb1996@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -24,7 +24,7 @@ declare class QuickLRU<KeyType, ValueType>
 	/**
 	Simple ["Least Recently Used" (LRU) cache](https://en.m.wikipedia.org/wiki/Cache_replacement_policies#Least_Recently_Used_.28LRU.29).
 
-	The instance is [`iterable`](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Iteration_protocols) so you can use it directly in a [`for…of`](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/for...of) loop.
+	The instance is an [`Iterable`](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Iteration_protocols) of `[key, value]` pairs so you can use it directly in a [`for…of`](https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/for...of) loop.
 
 	@example
 	```
